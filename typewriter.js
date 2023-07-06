ce1605a54746facbdc02afb53ced45fbb3f0b500
Jerscape <jeremydutton5@gmail.com>
2023-07-06 13:02:56 -0400

    commiting typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,15 @@
-//testing repository 
+
+const sentence = "hello there from lighthouse labs"
+let count = 2000
+for(const char of sentence){
+  
+  setTimeout(() => {
+    process.stdout.write(char)
+    //console.log(char)
+
+  }, count)
+  count+=50
+  
+}
+console.log("\n")
+
